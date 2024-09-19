7299008ffe06697f68266a7754df9324d0c06ff3
Brian Kipruto Peakay <148191135+brianpeakay6320@users.noreply.github.com>
2024-09-19 14:16:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+const courseList = document.getElementById('course-list');
+
+// sample course data
+const courses = [
+  { id: 1, title: 'Course 1', description: 'This is course 1' },
+  { id: 2, title: 'Course 2', description: 'This is course 2' },
+  { id: 3, title: 'Course 3', description: 'This is course 3' },
+  { id: 4, title: 'Course 4', description: 'This is course 4' },
+  { id: 5, title: 'Course 5', description: 'This is course 5' },
+  { id: 6, title: 'Course 6', description: 'This is course 5' }
+]
